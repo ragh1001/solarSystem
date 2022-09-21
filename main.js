dc797fc096abb0742ab6a8edfd2673a1abd06412
Raghav sharma <raghav1676.be20@chitkara.edu.in>
2022-09-21 14:40:18 +0530

    solar system project

---
+++ main.js
@@ -0,0 +1,54 @@
+function calmercury(){
+    let weight = document.getElementById("calc").value;
+    let mercuryw = weight * 0.38;
+    alert("Your weight on Mercury is : " + mercuryw);
+}
+
+function calvenus(){
+    let weight = document.getElementById("calc").value;
+    let venusw = weight * 0.91;
+    alert("Your weight on Venus is : " + venusw);
+}
+
+function calearth(){
+    let weight = document.getElementById("calc").value;
+    let earthw = weight * 1;
+    alert("Your weight on Earth is : " + earthw);
+}
+
+function calmars(){
+    let weight = document.getElementById("calc").value;
+    let marsw = weight * 0.38;
+    alert("Your weight on Mars is : " + marsw);
+}
+
+function caljupiter(){
+    let weight = document.getElementById("calc").value;
+    let jupiterw = weight * 2.34;
+    alert("Your weight on Jupiter is : " + jupiterw);
+}
+
+function calsaturn(){
+    let weight = document.getElementById("calc").value;
+    let saturnw = weight * 1.06;
+    alert("Your weight on Saturn is : " + saturnw);
+}
+
+function caluranus() {
+    let weight = document.getElementById("calc").value;
+    let uranusw = weight * 0.92;
+    alert("Your weight on Uranus is : " + uranusw);
+}
+
+function calneptune(){
+    let weight = document.getElementById("calc").value;
+    let neptunew = weight * 1.19;
+    alert("Your weight on Neptune is : " + neptunew);
+}
+
+function calpluto(){
+    let weight = document.getElementById("calc").value;
+    let plutow = weight * 0.06;
+    alert("Your weight on Pluto is : " + plutow);
+}
+
